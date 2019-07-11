d3ceb8f8266664804724e5d506c153cbe1791a21
Asif Hasan <d.asif.hasan@gmail.com>
2019-07-11 14:28:59 -0500

    Fix #3: A shell script to install & run Jenkins as a docker container

---
+++ jenkins/install-as-docker-container.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker run \
+  -u root \
+  --rm \
+  -d \
+  -p 7070:8080 \
+  -p 50000:50000 \
+  -v $HOME/jenkins-home:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  --name jenkins \
+  jenkinsci/blueocean
