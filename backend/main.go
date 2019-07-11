66c060fd123bc3b8f36039057134714f2aa375f6
Asif Hasan <d.asif.hasan@gmail.com>
2019-07-11 18:04:30 -0500

    A commit to test Jenkins build pipeline

--- backend/main.go
+++ backend/main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"log"
+	"net/http"
 )
 
 func handleRoot(writer http.ResponseWriter, request *http.Request) {
 	fmt.Println("new request from host", request.Host)
-	fmt.Fprintf(writer, "Yes, server is ready to serve!")
+	response := "Yes, server is ready to serve!"
+	fmt.Fprintf(writer, response)
 }
 
-func main(){
+func main() {
 	fmt.Println("starting server...")
 	http.HandleFunc("/", handleRoot)
 	log.Fatal(http.ListenAndServe(":8080", nil))
